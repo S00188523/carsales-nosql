8ad3b793d2685ea3f089a43d34a85772261ca529
Adam Grace <S00188523@atu.iem>
2023-04-18 19:59:19 +0100

    Routes Added to about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { CarlistComponent } from './components/carlist/carlist.component';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [	
+  { path: '', component: CarlistComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    CarlistComponent
+    CarlistComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
